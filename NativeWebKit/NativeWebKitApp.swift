e47a37c33df20fe4acffeee67956d784a6868d2f
Zack <zack@ukaton.com>
2023-12-20 15:51:55 -0800

    Initial Commit

---
+++ NativeWebKit/NativeWebKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeWebKitApp.swift
+//  NativeWebKit
+//
+//  Created by Zack Qattan on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NativeWebKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
